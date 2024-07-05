7ca6252b1bf187e3a17c5214aaab502f60eb276b
Leonid Zelov <112327440+LeoniD994@users.noreply.github.com>
2024-07-05 23:19:24 +0300

    fix description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Feedback from "./Feedback/Feedback";
 import Options from "./Options/Options";
 import Notification from "./Notification/Notification";
+import Description from './Description/Description';
 import css from "./App.module.css";
 
 const App = () => {
@@ -33,11 +34,7 @@ const App = () => {
 
   return (
     <div className={css.app}>
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <Description />
       <Options
         updateFeedback={updateFeedback}
         totalFeedback={totalFeedback}
